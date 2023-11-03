e5606ec775d7cbbab36009b07869eb1b8c19af75
Ho Nyeong <dev.honing@gmail.com>
2023-11-03 15:42:47 +0900

    Refactor: 주석 삭제

--- public/chat.js
+++ public/chat.js
@@ -20,26 +20,3 @@ function addText()  {
     .getElementById('Container')
     .appendChild(li);
 }
-
-// function addText()  {
-  
-//   // 1. 추가할 값을 input창에서 읽어온다
-//   const textBox 
-//     = document.getElementById('textBox').value;
-  
-//   // 2. 추가할 li element 생성
-//   // 2-1. 추가할 li element 생성
-//   const li = document.createElement("li");
-  
-//   // 2-2. li에 id 속성 추가 
-//   li.setAttribute('id',textBox);
-  
-//   // 2-3. li에 text node 추가 
-//   const textNode = document.createTextNode(textBox);
-//   li.appendChild(textNode);
-  
-//   // 3. 생성된 li를 ul에 추가
-//   document
-//     .getElementById('Container')
-//     .appendChild(li);
-// }
