21040999468369c5f459c1cdefaf91a551483da5
Ho Nyeong <dev.honing@gmail.com>
2023-11-03 16:30:57 +0900

    Fail: 3가지 span 태그를 만들기 때문에 함수화 하려 했지만 실패

--- public/chat.js
+++ public/chat.js
@@ -7,7 +7,7 @@ function addText()  {
   const li = document.createElement("li");
   
   // li에 id 속성을 추가
-  li.setAttribute('id',textBox);
+  li.setAttribute("id",textBox);
   
   // li에 textNode 추가
   const textNode = document.createTextNode(textBox);
@@ -15,7 +15,30 @@ function addText()  {
   
   // 생성된 li를 ul(Container)에 추가
   document
-    .getElementById('Container')
+    .getElementById("Container")
     .appendChild(li);
+
+  // span 태그 => 프로필 / 입력한 텍스트 / 시간
+  makeSpan ((profile, text, timeStamp) => {
+    const span = document.createElement("span")
+    if (profile) {
+      span.setAttribute("id", textBox)
+      span.appendChild(textNode)
+      li.appendChild(span)
+    }
+    else if (text) {
+      span.setAttribute("id", textBox)
+      span.appendChild(textNode)
+      li.appendChild(span)
+    }
+    else if (timeStamp) {
+      span.setAttribute("id", textBox)
+      span.appendChild(textNode)
+      li.appendChild(span)
+    }
+  } ) 
+  
+
 }
+
 
