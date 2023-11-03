12260ecc3fbfb9d6d226bd0b6850bd8c5a5933b2
Ho Nyeong <dev.honing@gmail.com>
2023-11-03 16:44:55 +0900

    Feat: 동적으로 생성되는 <li> 내부에 <span> 3가지를 자식 요소로 생성
    spanProfile
    spanText
    spanTimeStamp를 추가하고
    spanText에는 사용자가 입력한 요소를 불러오도록 설정함

--- public/chat.js
+++ public/chat.js
@@ -3,42 +3,33 @@ function addText()  {
   // input 태그에 입력된 문자열에 접근
   const textBox = document.getElementById('textBox').value;
   
-  // li 태그 생성
+  // li 태그 생성 // spanProfile, spanText, spanTimeStamp 추가
   const li = document.createElement("li");
+  const spanProfile = document.createElement("span")
+  const spanText = document.createElement("span")
+  const spanTimeStamp = document.createElement("span")
   
-  // li에 id 속성을 추가
-  li.setAttribute("id",textBox);
+  // li에 id 속성을 추가 // spanProfile, spanText, spanTimeStamp 추가
+  li.setAttribute("id", textBox);
+  spanProfile.setAttribute("id-profile", textBox)
+  spanText.setAttribute("id-text", textBox)
+  spanTimeStamp.setAttribute("id-timestamp", textBox)
   
-  // li에 textNode 추가
+  // li에 textNode 추가 // spanText도 추가하기
   const textNode = document.createTextNode(textBox);
   li.appendChild(textNode);
+  spanText.appendChild(textNode)
   
   // 생성된 li를 ul(Container)에 추가
   document
     .getElementById("Container")
     .appendChild(li);
 
-  // span 태그 => 프로필 / 입력한 텍스트 / 시간
-  makeSpan ((profile, text, timeStamp) => {
-    const span = document.createElement("span")
-    if (profile) {
-      span.setAttribute("id", textBox)
-      span.appendChild(textNode)
-      li.appendChild(span)
-    }
-    else if (text) {
-      span.setAttribute("id", textBox)
-      span.appendChild(textNode)
-      li.appendChild(span)
-    }
-    else if (timeStamp) {
-      span.setAttribute("id", textBox)
-      span.appendChild(textNode)
-      li.appendChild(span)
-    }
-  } ) 
-  
-
+  // li에 span 요소들 추가
+  li.appendChild(spanProfile)    
+  li.appendChild(spanText)    
+  li.appendChild(spanTimeStamp)    
+   
 }
 
 
