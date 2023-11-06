7563ce08f1f535a8a610056649f1599fca336726
Ho Nyeong <dev.honing@gmail.com>
2023-11-06 14:57:36 +0900

    Fix: issue#16에 등록된 문제 해결완료
    - 문자열 입력을 완료하면 문자열 입력창이 초기화 되도록 버그 픽스 완료

--- frontEnd/index.js
+++ frontEnd/index.js
@@ -1,32 +1,30 @@
-// 사용자가 입력한 문자열을 li 태그로 동적 생성하는 함수
-function addText()  {
+function addText() {
   // input 태그에 입력된 문자열에 접근
-  const textBox = document.getElementById('textBox').value;
+  const textBox = document.getElementById('textBox');
+  const inputValue = textBox.value;
   
-  // li 태그 생성 // spanProfile, spanText, spanTimeStamp 추가
+  // li 태그 생성
   const li = document.createElement("li");
-  const spanProfile = document.createElement("span")
-  const spanText = document.createElement("span")
-  const spanTimeStamp = document.createElement("span")
-  
-  // 클래스 지정
+
+  // span 요소 생성 및 클래스 지정
+  const spanProfile = document.createElement("span");
   spanProfile.classList.add("profile");
-  spanText.classList.add("text")
-  spanTimeStamp.classList.add("timeStamp")
 
-  
-  // li에 textNode 추가 // spanText도 추가하기
-  const textNode = document.createTextNode(textBox);
-  li.appendChild(textNode);
-  spanText.appendChild(textNode)
-  
+  const spanText = document.createElement("span");
+  spanText.classList.add("text");
+  spanText.textContent = inputValue; // 입력된 문자열을 spanText에 설정
+
+  const spanTimeStamp = document.createElement("span");
+  spanTimeStamp.classList.add("timeStamp");
+
+  // 생성된 span 요소들을 li에 추가
+  li.appendChild(spanProfile);
+  li.appendChild(spanText);
+  li.appendChild(spanTimeStamp);
+
   // 생성된 li를 ul(Container)에 추가
-  document
-    .getElementById("Container")
-    .appendChild(li);
+  document.getElementById("Container").appendChild(li);
 
-  // li에 span 요소들 추가
-  li.appendChild(spanProfile)    
-  li.appendChild(spanText)    
-  li.appendChild(spanTimeStamp)    
+  // 입력창 비우기
+  textBox.value = '';
 }
