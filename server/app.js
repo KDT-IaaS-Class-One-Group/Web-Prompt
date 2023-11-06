187bd35f1b1596754a2d12dc64a9df0bbe8bf821
Ho Nyeong <dev.honing@gmail.com>
2023-11-06 12:56:10 +0900

    Refactor: 미들웨어 설정(상대 경로)
    작성한 또다른 방식은 삭제

--- server/app.js
+++ server/app.js
@@ -8,12 +8,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 // 정적 파일 제공을 위한 미들웨어 설정 (상대 경로 사용)
-// 1. 
 app.use(express.static(path.join(__dirname, '..', 'frontEnd')));
-
-// 2. 
-app.use(express.static(path.join(__dirname, '../frontEnd')));
-
 
 const server = app.listen(port, () => {
   console.log(`서버가 http://localhost:${port} 에서 실행 중입니다.`);
