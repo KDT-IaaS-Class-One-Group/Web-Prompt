ed85024c785505e0e74bc1d4f0ddd058833ba294
Ho Nyeong <dev.honing@gmail.com>
2023-11-07 16:25:26 +0900

    Bug: fetch 방식으로 html을 로드하지 못함

--- frontEnd/index.js
+++ frontEnd/index.js
@@ -7,85 +7,98 @@ function addText() {
   }
 
   const container = document.getElementById('Container');
+
+  // 현재 시간을 24시간 형식으로 표시하기 위한 변수들
   const currentTime = new Date();
   const userHours = currentTime.getHours().toString().padStart(2, '0');
   const userMinutes = currentTime.getMinutes().toString().padStart(2, '0');
 
+  // user 메시지 생성
   const userLi = document.createElement("li");
   userLi.classList.add("message", "user");
-  userLi.style.backgroundColor = "#add8e6";
+  userLi.style.backgroundColor = "#add8e6"; // 라이트 블루 (#add8e6)
+
   const userSpanProfile = document.createElement("span");
   userSpanProfile.classList.add("profile");
   userSpanProfile.textContent = "👤 User: ";
+
   const userSpanText = document.createElement("span");
   userSpanText.classList.add("text");
   userSpanText.textContent = `${inputValue}`;
+
   const userSpanTimeStamp = document.createElement("span");
   userSpanTimeStamp.classList.add("timeStamp");
-  userSpanTimeStamp.style.float = "right";
-  userSpanTimeStamp.textContent = `${userHours}:${userMinutes}`;
+  userSpanTimeStamp.style.float = "right"; // 우측 정렬
+  userSpanTimeStamp.textContent = `${userHours}:${userMinutes}`; // 24시간 형식으로 표시
 
   userLi.appendChild(userSpanProfile);
   userLi.appendChild(userSpanText);
   userLi.appendChild(userSpanTimeStamp);
   container.appendChild(userLi);
 
+  // Assistant 대응 메시지 생성
   const assistantResponse = inputValue + "에 대한 제 생각은...";
+
+  // 현재 시간을 24시간 형식으로 표시하기 위한 변수들
   const assistantHours = currentTime.getHours().toString().padStart(2, '0');
   const assistantMinutes = currentTime.getMinutes().toString().padStart(2, '0');
 
   const assistantLi = document.createElement("li");
   assistantLi.classList.add("message", "assistant");
-  assistantLi.style.backgroundColor = "#90ee90";
+  assistantLi.style.backgroundColor = "#90ee90"; // 라이트 그린 (#90ee90)
+
   const assistantSpanProfile = document.createElement("span");
   assistantSpanProfile.classList.add("profile");
   assistantSpanProfile.textContent = "🌐 Assistant: ";
+
   const assistantSpanText = document.createElement("span");
   assistantSpanText.classList.add("text");
   assistantSpanText.textContent = assistantResponse;
+
   const assistantSpanTimeStamp = document.createElement("span");
   assistantSpanTimeStamp.classList.add("timeStamp");
-  assistantSpanTimeStamp.style.float = "right";
-  assistantSpanTimeStamp.textContent = `${assistantHours}:${assistantMinutes}`;
+  assistantSpanTimeStamp.style.float = "right"; // 우측 정렬
+  assistantSpanTimeStamp.textContent = `${assistantHours}:${assistantMinutes}`; // 24시간 형식으로 표시
 
   assistantLi.appendChild(assistantSpanProfile);
   assistantLi.appendChild(assistantSpanText);
   assistantLi.appendChild(assistantSpanTimeStamp);
   container.appendChild(assistantLi);
 
+  // 입력창 비우기
   textBox.value = "";
 }
 
+// 햄버거 메뉴
 const hamburgerMenu = document.getElementById("Hamburger-Menu");
-const sideBar = document.querySelector(".sideBar");
-const menuItems = ["home", "profile", "settings", "logout"];
-const closeButton = document.getElementById("Close-Button");
+const sideBar = document.createElement("div");
+sideBar.classList.add("sideBar");
 
-function toggleSideBar() {
-  sideBar.classList.toggle("open");
-  if (sideBar.classList.contains("open")) {
-    document.body.style.overflow = "hidden";
-  } else {
-    document.body.style.overflow = "auto";
-  }
-}
-
-hamburgerMenu.addEventListener("click", toggleSideBar);
-closeButton.addEventListener("click", toggleSideBar);
+const menuItems = ["Home", "Profile", "Settings", "Logout"];
 
 menuItems.forEach((item) => {
-  const button = document.getElementById(`${item}Button`);
-  button.addEventListener("click", () => {
-    window.location.href = `/pages/${item}`;
-    toggleSideBar();
-  });
+  const button = document.createElement("button");
+  button.textContent = item;
+  sideBar.appendChild(button);
 });
+
+const closeButton = document.createElement("button");
+closeButton.innerHTML = "&#9776;"; // ☰ 아이콘
+closeButton.classList.add("hamburgerIcon");
+hamburgerMenu.appendChild(sideBar);
+
+const toggleSideBar = () => {
+  sideBar.classList.toggle("open");
+};
+
+hamburgerMenu.addEventListener("click", toggleSideBar);
+
+closeButton.addEventListener("click", toggleSideBar);
 
 document.addEventListener("DOMContentLoaded", () => {
   const sideBar = document.querySelector(".sideBar");
 
-  const menuItems = ["Home", "Profile", "Settings", "Logout"];
-  const hamburgerMenu = document.getElementById("Hamburger-Menu");
+  sideBar.style.height = `calc(100vh - ${headerHeight + footerHeight}px)`;
 
   menuItems.forEach((item) => {
     const button = document.getElementById(`${item.toLowerCase()}Button`);
