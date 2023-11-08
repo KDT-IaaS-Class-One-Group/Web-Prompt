c4984c01fdca1906a27dfd06ae43dfb769648936
Ho Nyeong <dev.honing@gmail.com>
2023-11-08 15:06:45 +0900

    Feat: 서버에 전송한 문자열을 promptData.json에 저장하는 로직 추가

--- server/route.js
+++ server/route.js
@@ -26,6 +26,47 @@ router.post('/submit', (req, res) => {
   const inputValue = req.body.inputText;
   console.log('사용자 입력 내용:', inputValue);
   res.json({ message: 'POST 요청을 받았습니다!' });
+
+  // json 파일을 읽고 편집하는 기능 추가
+
+  // 파일 읽기 (비동기 방식)
+  const filePath = path.join(__dirname, 'promptData.json');
+  fs.readFile(filePath, 'utf8', (err, data) => {
+    if (err) {
+      console.error('파일 읽기 에러:', err);
+      return res.status(500).json({ error: '파일 읽기 에러' });
+    }
+
+    let jsonData = [];
+
+    if (data) {
+      // 읽어온 데이터가 있다면 JSON으로 파싱
+      jsonData = JSON.parse(data);
+    }
+
+    // 새로운 데이터 생성 (사용자 입력 값만 포함)
+    const newEntry = {
+      inputValue: inputValue
+    };
+
+    // JSON 파일에 데이터 추가 (비동기 방식)
+    jsonData.push(newEntry);
+
+    // JSON 데이터를 다시 JSON 문자열로 변환
+    const updatedJsonData = JSON.stringify(jsonData, null, 2);
+
+    // 파일에 쓰기 (비동기 방식)
+    fs.writeFile(filePath, updatedJsonData, 'utf8', (err) => {
+      if (err) {
+        console.error('파일 쓰기 에러:', err);
+        return res.status(500).json({ error: '파일 쓰기 에러' });
+      }
+
+      console.log('데이터가 JSON 파일에 추가되었습니다.');
+      res.json({ message: 'POST 요청을 받았습니다!' });
+    });
+  });
 });
+
 
 module.exports = router;
