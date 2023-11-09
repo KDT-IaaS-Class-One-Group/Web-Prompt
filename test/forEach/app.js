38fbb7cf149714344160efd2ef7725b21be0f0a6
Ho Nyeong <dev.honing@gmail.com>
2023-11-09 15:11:57 +0900

    Practice: forEach() 메서드를 심화학습
    - 유의할 점 발견해서 주석 처리

--- test/forEach/app.js
+++ test/forEach/app.js
@@ -1,9 +1,14 @@
-const numbers = [1, 2, 3, 4, 5];
+const users = [
+  { name: "Alice", age: 25 },
+  { name: "Bob", age: 17 },
+  { name: "Charlie", age: 22 },
+  { name: "David", age: 30 }
+];
 
-console.log("forEach() 연습하기:");
-console.log("짝수만 출력하기:");
-numbers.forEach(number => {
-  if (number % 2 === 0) { // 나머지가 0인 number를 출력
-    console.log(number);
+console.log("성인 사용자 정보 출력하기:");
+users.forEach(user => {
+  if (user.age >= 18) { // age가 18세 이상인 조건을 충족하는 애들을 매개변수 user에 담기
+    console.log(users); // ! 이렇게 하면 forEach() 메서드 자체가 배열을 순회하기 때문에 동일한 객체가 두 번 찍힌다.
+    // console.log(`이름: ${user.name}, 나이: ${user.age}`); 
   }
 });
