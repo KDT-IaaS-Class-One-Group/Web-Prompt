9bdbe704def77391456ffc4f6e4cbbaa45583588
Ho Nyeong <dev.honing@gmail.com>
2023-11-09 16:50:10 +0900

    Refactor: route.js 리팩터링 완료

--- server/route.js
+++ server/route.js
@@ -23,36 +23,34 @@ router.get('/logout', (req, res) => {
 
 // POST 요청 처리
 router.post('/submit', (req, res) => {
-  const inputValue = req.body.inputText;
+  const inputValue = req.body.inputText; // POST 요청에서 전송된 데이터 중에서 inputText라는 이름의 필드에 해당하는 값을 가져옴
   console.log('사용자 입력 내용:', inputValue);
 
   // json 파일을 읽고 편집하는 기능 추가
 
   // 파일 읽기 (비동기 방식)
-  const filePath = path.join(__dirname, 'promptData.json');
+  const filePath = path.join(__dirname, 'promptData.json'); // path.join(): 여러 개의 문자열을 하나의 경로로 결합
   fs.readFile(filePath, 'utf8', (err, data) => {
     if (err) {
       console.error('파일 읽기 에러:', err);
       return res.status(500).json({ error: '파일 읽기 에러' });
     }
 
-    let jsonData = [];
+    let jsonData = []; // jsonData는 빈 배열에 요소가 추가되고 변경되므로 let으로 변수 선언
 
     if (data) {
-      // 읽어온 데이터가 있다면 JSON으로 파싱
-      jsonData = JSON.parse(data);
+      jsonData = JSON.parse(data); // 읽어온 데이터가 있다면 JSON으로 파싱
     }
 
-    // 새로운 데이터 생성 (사용자 입력 값만 포함)
-    const newEntry = {
-      inputValue: inputValue
+    const newEntry = { // 사용자가 입력한 값을 객체에 담음
+      inputText: inputValue
     };
 
     // JSON 파일에 데이터 추가 (비동기 방식)
     jsonData.push(newEntry);
 
     // JSON 데이터를 다시 JSON 문자열로 변환
-    const updatedJsonData = JSON.stringify(jsonData, null, 2);
+    const updatedJsonData = JSON.stringify(jsonData, null, 2); //? 
 
     // 파일에 쓰기 (비동기 방식)
     fs.writeFile(filePath, updatedJsonData, 'utf8', (err) => {
@@ -60,7 +58,7 @@ router.post('/submit', (req, res) => {
         console.error('파일 쓰기 에러:', err);
         return res.status(500).json({ error: '파일 쓰기 에러' });
       }
-
+      // 서버 콘솔에 출력
       console.log('데이터가 JSON 파일에 추가되었습니다.');
       res.json({ message: 'POST 요청을 받았습니다!' });
     });
