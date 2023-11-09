41b8692412ec1749281c61770f7b9eca97a1394d
Ho Nyeong <dev.honing@gmail.com>
2023-11-09 15:13:31 +0900

    Practice: forEach() 메서드 심화학습
    - 조건문에 해당하는 정보만 출력하기

--- test/forEach/app.js
+++ test/forEach/app.js
@@ -8,7 +8,6 @@ const users = [
 console.log("성인 사용자 정보 출력하기:");
 users.forEach(user => {
   if (user.age >= 18) { // age가 18세 이상인 조건을 충족하는 애들을 매개변수 user에 담기
-    console.log(users); // ! 이렇게 하면 forEach() 메서드 자체가 배열을 순회하기 때문에 동일한 객체가 두 번 찍힌다.
-    // console.log(`이름: ${user.name}, 나이: ${user.age}`); 
+    console.log(`이름: ${user.name}, 나이: ${user.age}`); 
   }
 });
