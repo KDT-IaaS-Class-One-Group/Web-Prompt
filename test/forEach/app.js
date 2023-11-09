eca069a2d22f454f1510e0c9c8f78c7db0291b46
Ho Nyeong <dev.honing@gmail.com>
2023-11-09 15:03:42 +0900

    forEach() 메서드로 짝수만 출력해보기

--- test/forEach/app.js
+++ test/forEach/app.js
@@ -1,6 +1,9 @@
 const numbers = [1, 2, 3, 4, 5];
 
 console.log("forEach() 연습하기:");
+console.log("짝수만 출력하기:");
 numbers.forEach(number => {
-  console.log(number);
+  if (number % 2 === 0) { // 나머지가 0인 number를 출력
+    console.log(number);
+  }
 });
